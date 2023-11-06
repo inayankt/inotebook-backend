28345ecdcd87a24453d28d79e3c3e52a6a9dd871
Nayan Kumar <ind.nayan4729@gmail.com>
2023-11-07 01:19:51 +0530

    Added validation using express-validator

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const port = parseInt(process.env.API_PORT);
 // Connecting to MongoDB
 connectToMongo();
 
+// Middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 // Available routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("hello auth");
+// Create a user: "POST /api/auth/". Doesn't require auth
+router.post("/",[
+    body("email", "Please enter a valid email.").isEmail().custom(async (value) => {
+        const user = await User.findOne({email: value});
+        if(user){
+            return Promise.reject("Email already in use.");
+        }
+    }),
+    body("name", "Length of name must be atleast 5 characters.").isLength({min: 5}),
+    body("password", "Password must be atleast 6 characters long.").isLength({min: 6})
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        res.json(user);
+    } catch(err) {
+        res.status(500).json({error: err.message});
+    }
 });
 
 module.exports = router;
